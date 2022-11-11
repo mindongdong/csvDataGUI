6d885253468201ade73816d93aa41de5613de601
Jisu Kang <kangjs0237@gmail.com>
2022-11-11 12:30:49 +0900

    feat: barPlot
    update barPlot

--- frontend/src/components/barPlot.vue
+++ frontend/src/components/barPlot.vue
@@ -1,58 +1,124 @@
+<!-- eslint-disable -->
 <template>
   <div class="layout">
-    <vue-csv-import v-model="csv" :map-fields="['Values', 'Annotation']">
-      <vue-csv-toggle-headers></vue-csv-toggle-headers>
-      <vue-csv-errors></vue-csv-errors>
-      <vue-csv-input></vue-csv-input>
-      <vue-csv-map :auto-match="false"></vue-csv-map>
+    <vue-csv-import 
+      v-model="csv" 
+      :map-fields="['Values', 'Annotation']">
+
     </vue-csv-import>
-    <!-- <form class="uploadCsv">
-      <label class="uploadCsv__button">
-        파일 찾기
-        <input
-          df-file
-          class="uploadCsv__input"
-          type="file"
-          ref="selectFile"
-          @change.prevent="previewFile"
-          accept="text/csv"
-        />
-      </label>
-      <input
-        class="uploadCsv__info"
-        placeholder="첨부파일"
-        v-if="selectFile"
-        v-model="selectFile.name"
-        readonly
-      />
-      <input class="uploadCsv__info" placeholder="첨부파일" v-else readonly />
-    </form> -->
+
     <div class="canvas">
-      bar plot
       <div>{{ csv }}</div>
+      <p>bar plot</p>
+      <!-- <div class=".canvas"></div>   -->
+      <svg></svg>    
     </div>
   </div>
 </template>
 
 <script>
+/* eslint-disable */
 import { VueCsvImport } from "vue-csv-import";
-
+import * as d3 from "d3";
 export default {
   components: {
     VueCsvImport,
   },
   data() {
     return {
-      csv: null,
+      csv: [],
     };
   },
-  methods: {
-    // previewFile() {
-    //   if (this.$refs.selectFile.files.length > 0) {
-    //     this.selectFile = this.$refs.selectFile.files[0];
-    //   }
-    // },
+  mounted() {
+      
   },
+  watch: {
+    csv: function() {
+
+      console.log("csv Changed")
+
+      // d3.select("body").selectAll("canvas").remove();
+      d3.selectAll("svg > *").remove();
+
+      // if(typeof this.csv[0] != 'number')
+      //   this.csv.shift(); 
+
+      const width = 400
+      const height = 400
+      let [mt, mb, mr, ml] = [30, 50, 50, 80]
+
+      const svg = d3.select('svg')
+                    .attr('width', width)
+                    .attr('height', height )
+
+      const graphWidth = width - ml - mr
+      const graphHeight = height - mt - mb
+
+      const graph = svg.append('g')
+                      .attr('width', graphWidth)
+                      .attr('height', graphHeight)
+                      .attr('transform', `translate(${ml}, ${mt})`)
+
+      const xAxisG = graph.append('g')
+                          .attr('transform', `translate(0, ${graphHeight})`)
+      const yAxisG = graph.append('g')
+
+      const x = d3.scaleBand()
+                  .domain(this.csv.map(function(d) { return d.Values;} ))
+                  .range([0, graphWidth])
+                  .padding(0.25)
+      // // console.log(item.Values)
+      //console.log(x)
+      const y = d3.scaleLinear()
+                  .domain([0, d3.max(this.csv, d => d.Annotation)])
+                  .range([graphHeight, 0])
+
+      const bars = graph.selectAll('rect')
+                      .data(this.csv);
+
+      bars.enter()
+          .append('rect')
+          .attr('height', function(d) {return (graphHeight - y(d.Annotation))})
+          //.attr('height', graphHeight - this.csv.Annotation)
+          .attr('width', x.bandwidth)
+          .attr('x', function(d) {return x(d.Values)})
+          .attr('y', function(d) {return y(d.Annotation)})
+
+
+      bars.enter()
+          .append('text')
+          .attr('x', function(d) {return x(d.Values)})
+          .attr("y", function(d) {return y(d.Annotation) - 5})
+          .text(function(d) {return d.Annotation})
+          .style('font-size', '12px')
+
+      svg.append("text")
+        .attr("text-anchor", "end")
+        .attr("x", width/2 + ml/2)
+        .attr("y", height + mt - 45)
+        .text("X label");
+
+      svg.append("text")
+          .attr("text-anchor", "end")
+          .attr("transform", "rotate(-90)")
+          .attr("y", ml/2 - 5)
+          .attr("x", - height/2 + 20)
+          .text("Y label")
+
+        const xAxis = d3.axisBottom(x)
+        const yAxis = d3.axisLeft(y)
+        
+        xAxisG.call(xAxis)
+        yAxisG.call(yAxis)
+
+        xAxisG.selectAll('text')
+            .attr('fill', 'blue')
+            .attr('transform', 'rotate(-45)')
+            .attr('text-anchor', 'end')       
+        
+
+    },
+  }
 };
 </script>
 
