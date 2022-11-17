ef20a2976a530786084b08c6ebb22c7472b43a9e
Jisu Kang <kangjs0237@gmail.com>
2022-11-17 21:34:16 +0900

    update barPlot
    update barPlot

--- frontend/src/components/barPlot.vue
+++ frontend/src/components/barPlot.vue
@@ -3,8 +3,8 @@
   <div class="layout">
     <vue-csv-import 
       v-model="csv" 
-      :map-fields="['Values', 'Annotation']">
-
+      :map-fields="['annotation', 'values']">
+      
     </vue-csv-import>
 
     <div class="canvas">
@@ -24,6 +24,10 @@ export default {
   data() {
     return {
       csv: [],
+      count: 0,
+      X_label: "X label",
+      Y_label: "Y_label",
+      arr_len: 0,
     };
   },
   mounted() {
@@ -32,16 +36,29 @@ export default {
   watch: {
     csv: function() {
 
-      console.log("csv Changed")
-
-      // d3.select("body").selectAll("canvas").remove();
       d3.selectAll("svg > *").remove();
 
-      // if(typeof this.csv[0] != 'number')
-      //   this.csv.shift(); 
+      //코드수정중..
+      if(this.csv[this.csv.length - 1] != undefined && this.count == 0){
 
-      const width = 400
-      const height = 400
+        this.X_label = this.csv[0].annotation;
+        this.Y_label = this.csv[0].values;
+        
+        this.csv.shift();
+        this.count = this.count+1
+        this.arr_len = this.csv.length;
+      }
+
+      //변화할때..
+      if (this.arr_len != this.csv.length){
+          this.X_label = this.csv[0].annotation;
+          this.Y_label = this.csv[0].values;
+          this.csv.shift();
+          this.arr_len = this.csv.length;
+      }
+
+      const width = 750
+      const height = 330
       let [mt, mb, mr, ml] = [30, 50, 50, 80]
 
       const svg = d3.select('svg')
@@ -58,53 +75,61 @@ export default {
 
       const xAxisG = graph.append('g')
                           .attr('transform', `translate(0, ${graphHeight})`)
-      const yAxisG = graph.append('g')
-
+      const yAxisG = graph.append('g')     
+                //원래
       const x = d3.scaleBand()
-                  .domain(this.csv.map(function(d) { return d.Values;} ))
+                  .domain(this.csv.map(function(d) {return d.annotation;} ))
                   .range([0, graphWidth])
                   .padding(0.25)
-      // // console.log(item.Values)
-      //console.log(x)
+
       const y = d3.scaleLinear()
-                  .domain([0, d3.max(this.csv, d => d.Annotation)])
+                  .domain([0, d3.max(this.csv, d => d.values)])
                   .range([graphHeight, 0])
 
       const bars = graph.selectAll('rect')
                       .data(this.csv);
 
-      bars.enter()
+
+      this.csv.forEach(function (item, index){
+        bars.enter()
           .append('rect')
-          .attr('height', function(d) {return (graphHeight - y(d.Annotation))})
-          //.attr('height', graphHeight - this.csv.Annotation)
+          .attr('height', function(d) {return (graphHeight - y(d.values))})
           .attr('width', x.bandwidth)
-          .attr('x', function(d) {return x(d.Values)})
-          .attr('y', function(d) {return y(d.Annotation)})
+          // .attr('width',20)
+          // console.log(graphWidth/d.annotation.length * index); 
+          .attr('x', function(d,i) { console.log(x(d.annotation[i])); return x(d.annotation)})
+          // .attr('x', function(d) { return 40*index  + 100})
+          .attr('y', function(d) {return y(d.values)})
+      })
+
+
+      
 
 
       bars.enter()
           .append('text')
-          .attr('x', function(d) {return x(d.Values)})
-          .attr("y", function(d) {return y(d.Annotation) - 5})
-          .text(function(d) {return d.Annotation})
+          .attr('x', function(d) {return x(d.annotation)})
+          .attr("y", function(d) {return y(d.values) - 5})
+          .text(function(d) {return d.values})
           .style('font-size', '12px')
 
       svg.append("text")
         .attr("text-anchor", "end")
         .attr("x", width/2 + ml/2)
         .attr("y", height + mt - 45)
-        .text("X label");
+        .text(this.X_label);
 
       svg.append("text")
           .attr("text-anchor", "end")
           .attr("transform", "rotate(-90)")
           .attr("y", ml/2 - 5)
           .attr("x", - height/2 + 20)
-          .text("Y label")
+          .text(this.Y_label)
 
         const xAxis = d3.axisBottom(x)
         const yAxis = d3.axisLeft(y)
         
+
         xAxisG.call(xAxis)
         yAxisG.call(yAxis)
 
@@ -112,8 +137,6 @@ export default {
             .attr('fill', 'blue')
             .attr('transform', 'rotate(-45)')
             .attr('text-anchor', 'end')       
-        
-
     },
   }
 };
